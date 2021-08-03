c0c7c3714f635c7bcd7aa20ba9dc043430c04a42
russelltheprogrammer <russelltheprogrammer@gmail.com>
2021-08-03 15:58:25 -0400

    Work on number buttons

--- src/JavascriptCalculator.js
+++ src/JavascriptCalculator.js
@@ -1,47 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import './index.scss';
 
 
 const JavascriptCalculator = () => {
+
+const [display, setDisplay] = useState(0);
+
+const numberButton = (e) => {
+    const number = e.target.value;
+    return setDisplay(display + number);
+}
+
     return ( 
     
-    <div class="container-fluid">
-        <table class="calculator-container" border="0" align="center" width="100%">
-        <tr>
-            <td colspan="5" id="display">DISPLAY</td>
-        </tr>
-        <tr>
-            <td><button id="seven" class="btn btn-secondary">7</button></td>
-            <td><button id="eight" class="btn btn-secondary">8</button></td>
-            <td><button id="nine" class="btn btn-secondary">9</button></td>
-            <td><button id="divide" class="btn btn-primary"><i class="fas fa-divide"></i></button></td>
-            <td><button id="clear" class="btn btn-danger">AC</button></td>
-        </tr>
-        <tr>
-            <td><button id="four" class="btn btn-secondary">4</button></td>
-            <td><button id="five" class="btn btn-secondary">5</button></td>
-            <td><button id="six" class="btn btn-secondary">6</button></td>
-            <td><button id="multiply" class="btn btn-primary"><i class="fas fa-times"></i></button></td>
-            <td rowspan="3"><button id="equal" class="btn btn-success"><i class="fas fa-equals"></i></button></td>
-        </tr>
-        <tr>
-            <td><button id="one" class="btn btn-secondary">1</button></td>
-            <td><button id="two" class="btn btn-secondary">2</button></td>
-            <td><button id="three" class="btn btn-secondary">3</button></td>
-            <td><button id="subtract" class="btn btn-primary"><i class="fas fa-minus"></i></button></td>
-        </tr>
-        <tr>
-            <td colspan="2"><button id="zero" class="btn btn-secondary">0</button></td>
-            <td><button id="decimal" class="btn btn-secondary">.</button></td>
-            <td><button id="add" class="btn btn-primary"><i class="fas fa-plus"></i></button></td>
-        </tr>
-
-
-
-
-
-
-
+    <div className="container-fluid">
+        <table className="calculator-container" border="0" align="center" width="100%">
+            <tbody>
+                <tr>
+                    <td colSpan="5" id="display">{display}</td>
+                </tr>
+                <tr>
+                    <td><button id="seven" className="btn btn-secondary" onClick={numberButton}>7</button></td>
+                    <td><button id="eight" className="btn btn-secondary" onClick={numberButton}>8</button></td>
+                    <td><button id="nine" className="btn btn-secondary" onClick={numberButton}>9</button></td>
+                    <td><button id="divide" className="btn btn-primary"><i className="fas fa-divide"></i></button></td>
+                    <td><button id="clear" className="btn btn-danger" onClick={() => setDisplay(0)}>AC</button></td>
+                </tr>
+                <tr>
+                    <td><button id="four" className="btn btn-secondary">4</button></td>
+                    <td><button id="five" className="btn btn-secondary">5</button></td>
+                    <td><button id="six" className="btn btn-secondary">6</button></td>
+                    <td><button id="multiply" className="btn btn-primary"><i className="fas fa-times"></i></button></td>
+                    <td rowSpan="3"><button id="equal" className="btn btn-success"><i className="fas fa-equals"></i></button></td>
+                </tr>
+                <tr>
+                    <td><button id="one" className="btn btn-secondary">1</button></td>
+                    <td><button id="two" className="btn btn-secondary">2</button></td>
+                    <td><button id="three" className="btn btn-secondary">3</button></td>
+                    <td><button id="subtract" className="btn btn-primary"><i className="fas fa-minus"></i></button></td>
+                </tr>
+                <tr>
+                    <td colSpan="2"><button id="zero" className="btn btn-secondary">0</button></td>
+                    <td><button id="decimal" className="btn btn-secondary">.</button></td>
+                    <td><button id="add" className="btn btn-primary"><i className="fas fa-plus"></i></button></td>
+                </tr>
+            </tbody>
         </table>
         
     </div> 
